d83921dda775af2a08acfb9c31b42dda323f04f1
Shelimar <abbi90@mail.ru>
2022-10-17 12:56:59 +0300

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,56 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = max = arr[0];
+  sum = 0;
 
-  // Ваш код
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i]
+  }
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
-}
-
+  let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+      sum += arr[i]
+    }
+    return sum;
+  }
+  
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
+  for(let i = 0; i < arrOfArr.length; i++) {
+    let oneArrSum = func(arrOfArr[i])
+    if(oneArrSum > max) { 
+      max = oneArrSum
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max;
+  min = max = arr[0]
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > min) {
+      min = arr[i]
+    }
+    if(arr[i] < max) {
+      max = arr[i]
+    }
+  }
+  return Math.abs(max-min);
 }
